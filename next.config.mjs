c18b94de0e9f1885bed753795f76e7cadd2ebeaf
Prathiba-hub <sd1768135@gmail.com>
2026-08-18 09:58:19 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['lucide-react', 'react-icons', 'recharts'],
-  },
-};
-
-export default nextConfig;
